86be3978146f8061a2cac4f8a89595766a1e83ec
lekha7788 <lekha.thunugunta@gmail.com>
2024-12-25 20:35:43 +0530

    Update SeleniumGridTest.java

--- src/test/java/SeleniumGridTest.java
+++ src/test/java/SeleniumGridTest.java
@@ -8,11 +8,8 @@ import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-
 public class SeleniumGridTest {
-
     private WebDriver driver;
-
     @DataProvider(name = "browsers")
     public Object[][] getBrowsers() {
         return new Object[][]{
@@ -21,26 +18,20 @@ public class SeleniumGridTest {
                 {"MicrosoftEdge"}
         };
     }
-
     @Test(dataProvider = "browsers")
-    public void testGoogleTitle(String browser) throws MalformedURLException {
+    public void testAmazonTitle(String browser) throws MalformedURLException {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setBrowserName(browser);
-
         // Replace localhost with your Docker host IP if necessary
         driver = new RemoteWebDriver(new URL("http://localhost:4444"), capabilities);
-
         // Navigate to Google
-        driver.get("https://www.google.com");
-
+        driver.get("https://www.amazon.com");
         // Verify the title of the page
-        String expectedTitle = "Google";
+        String expectedTitle = "Amazon.com. Spend less. Smile more.";
         String actualTitle = driver.getTitle();
         System.out.println("Title on " + browser + ": " + actualTitle);
-
         Assert.assertEquals(actualTitle, expectedTitle, "Title did not match on browser: " + browser);
     }
-
     @AfterMethod
     public void tearDown() {
         if (driver != null) {
